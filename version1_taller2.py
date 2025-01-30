45c33a1d54667659c44d41212131d1e60e2e58ac
Sara Gonzalez <ss.gonzalezc1@uniandes.edu.co>
2025-01-30 09:01:10 -0500

    agrega tamaño como variable n

--- version1_taller2.py
+++ version1_taller2.py
@@ -3,5 +3,6 @@ import numpy as np
 left= 2
 mode= 5
 right= 11
-vals = np.random.triangular(left, mode, right, size=1000)
+n=2000
+vals = np.random.triangular(left, mode, right, size=n)
 print(vals)
